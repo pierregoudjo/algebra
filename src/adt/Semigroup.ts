2f70132f52efc6f935e8c31edcdcb9b9f8b8983a
Pierre Goudjo <pierre.goudjo@merapar.com>
2020-11-18 13:10:13 +0100

    Add tuple and struct semigroup combinators and rename number semigroups

--- src/adt/Semigroup.ts
+++ src/adt/Semigroup.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-explicit-any
 declare const semigroup: unique symbol;
 
 export type Semigroup<T> = {
@@ -7,7 +8,24 @@ export type Semigroup<T> = {
 
 export const fromConcat = <T>(fn: (x: T, y: T) => T) => fn as Semigroup<T>;
 export const concatString: Semigroup<string> = fromConcat((x, y) => x + y);
-export const concatNumber: Semigroup<number> = fromConcat((x, y) => x + y);
+export const addNumber: Semigroup<number> = fromConcat((x, y) => x + y);
+export const multiplyNumber: Semigroup<number> = fromConcat((x, y) => x * y);
 export const concatArray: Semigroup<unknown[]> = fromConcat((x, y) =>
   x.concat(y)
 );
+
+export const getTupleSemigroup = <T extends ReadonlyArray<Semigroup<any>>>(
+  semigroups: T,
+): Semigroup<{ [K in keyof T]: T[K] extends Semigroup<infer A> ? A : never }> =>
+  fromConcat((x, y) =>
+    semigroups.map((semigroup, i) => semigroup(x[i], y[i])) as any
+  );
+
+export const getStructSemigroup = <T extends Record<string, unknown>>(
+  semigroupStruct: { [K in keyof T]: Semigroup<T[K]> },
+): Semigroup<T> =>
+  fromConcat((x, y) =>
+    Object.entries(semigroupStruct).map(([key, semigroup]) => ({
+      [key]: semigroup(x[key], y[key]),
+    })).reduce((prev, curr) => ({ ...prev, ...curr }), {}) as any
+  );
--- tests/adt/Semigroup.test.ts
+++ tests/adt/Semigroup.test.ts
@@ -1,16 +1,23 @@
 import {
   array,
   assert,
+  bigInt,
+  float,
+  genericTuple,
   integer,
   property,
+  record,
   string,
 } from "https://cdn.skypack.dev/fast-check";
 
 import { expect } from "https://deno.land/x/expect/mod.ts";
 import {
+  addNumber,
   concatArray,
-  concatNumber,
   concatString,
+  getStructSemigroup,
+  getTupleSemigroup,
+  multiplyNumber,
   Semigroup,
 } from "../../src/adt/Semigroup.ts";
 
@@ -22,20 +29,73 @@ const associativity = <T>(
     generator(),
     generator(),
     generator(),
-    (x: T, y: T, z: T) =>
-      // expect(equals(concat(x, concat(y, z)), concat(concat(x, y), z))).toBeTruthy()
-      expect(concat(x, concat(y, z))).toEqual(concat(concat(x, y), z)),
+    (x: T, y: T, z: T) => {
+      expect(concat(x, concat(y, z))).toEqual(concat(concat(x, y), z));
+    },
   );
 
-Deno.test("concatString is associative", () => {
+Deno.test("concatString is an associative binary operation on strings and concat two strings together ", () => {
   assert(associativity(concatString, string), undefined);
 });
 
-Deno.test("concatNumber is associative", () => {
-  assert(associativity(concatNumber, integer), undefined);
+Deno.test("addNumber is an associative binary operation on numbers and add two numbers together ", () => {
+  assert(associativity(addNumber, integer), undefined);
 });
 
-Deno.test("concatArray is associative", () => {
+Deno.test("multiplyNumber is an associative binary operation on numbers and multiply two numbers together ", () => {
+  assert(associativity(multiplyNumber, bigInt), undefined);
+  assert(associativity(multiplyNumber, float), undefined);
+});
+
+Deno.test("concatArray is an associative binary operation on arrays and concat two arrays together", () => {
   const generator = () => array(string());
   assert(associativity(concatArray, generator), undefined);
 });
+
+Deno.test(
+  `getTupleSemigroup generate an associative binary operation 
+that concats together the elements of a tuple`,
+  () => {
+    type Tuple = Readonly<[string, number]>;
+
+    const tuple = () => genericTuple([string(), integer()]);
+    const tupleConcatenation = getTupleSemigroup(
+      [concatString, addNumber] as const,
+    );
+    assert(associativity(tupleConcatenation, tuple), undefined);
+
+    assert(
+      property(
+        tuple(),
+        tuple(),
+        (x: Tuple, y: Tuple) => {
+          const result = tupleConcatenation(x, y);
+          expect(result[0]).toEqual(concatString(x[0], y[0]));
+          expect(result[1]).toEqual(addNumber(x[1], y[1]));
+        },
+      ),
+      undefined,
+    );
+  },
+);
+
+Deno.test(
+  `getStructSemigroup generates an associative binary operation 
+that concats together the elements of a record`,
+  () => {
+    type Point = { x: number; y: number };
+    const addCoordinates = getStructSemigroup({ x: addNumber, y: addNumber });
+    const point = () => record({ x: integer(), y: integer() }, undefined);
+
+    // assert(associativity(addCoordinates, point), undefined);
+
+    assert(
+      property(point(), point(), (a: Point, b: Point) => {
+        const newPoint = addCoordinates(a, b);
+        expect(newPoint.x).toEqual(addNumber(a.x, b.x));
+        expect(newPoint.y).toEqual(addNumber(a.y, b.y));
+      }),
+      undefined,
+    );
+  },
+);
